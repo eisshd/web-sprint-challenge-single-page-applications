f7e70c8fd5dac2f5b4654d4cb188c356994e599e
Harley Eiss <ice.eiss@gmail.com>
2023-06-09 01:18:45 -0400

    setting up elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import {React, useState} from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom' 
+import Home from "./Home";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <BrowserRouter>
+      <div>
+        <h1>Bloomtech Eats</h1>
+        <p>Where food for thought is one correct syntax away</p>
+        <nav className="nav">
+            <div className="nav_links">
+              <Link to='/' >Home</Link>
+            </div>
+        </nav>
+        <Routes>
+            <Route path='/'element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
+      
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,50 @@
+
+
+export default function Home() {
+    return(
+    <div className="home-container">
+        <img src="https://thumbs.dreamstime.com/b/laptop-notebook-computer-pizza-icon-sign-vector-illustration-113029243.jpg"/>
+        <div className="Pizzaz">
+            <div>
+                <h3>Pizza 1</h3>
+                <p>Pizza 1 description</p>
+                <p>Pizza Price</p>
+                <button> Place Your Order </button>
+            </div>
+            <div>
+                <h3>Pizza 2</h3>
+                <p>Pizza 2 description</p>
+                <p>Pizza Price</p>
+                <button id="order-pizza"> Place Your Order </button>
+            </div>
+            <div>
+                <h3>Pizza 3</h3>
+                <p>Pizza 3 description</p>
+                <p>Pizza Price</p>
+                <button id="order-pizza"> Place Your Order </button>
+            </div>
+            <div>
+                <h3>Pizza 4</h3>
+                <p>Pizza 4 description</p>
+                <p>Pizza Price</p>
+                <button id="order-pizza"> Place Your Order </button>
+            </div>
+            <div>
+                <h3>Pizza 5</h3>
+                <p>Pizza 5 description</p>
+                <p>Pizza Price</p>
+                <button id="order-pizza"> Place Your Order </button>
+            </div>
+            <div>
+                <h3>Pizza 6</h3>
+                <p>Pizza 6 description</p>
+                <p>Pizza Price</p>
+                <button id="order-pizza"> Place Your Order </button>
+            </div>
+        </div>
+        
+    </div>
+    
+    
+    )
+}
